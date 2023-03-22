5097a98e2740fcf89efdfa972bc267e0376a9538
Nihaal Sangha <nihaal.git@gmail.com>
2023-03-22 16:15:30 +0000

    Split status check logic from template rendering

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+mod status;
 mod teams;
+
+use status::{get_results, Protocol, RequestResult, TeamResult};
 
 use actix_web::{
     get,
@@ -8,43 +11,12 @@ use actix_web::{
 use handlebars::handlebars_helper;
 use serde::Serialize;
 use serde_json::json;
-use std::{
-    fs,
-    time::{Duration, SystemTime},
-};
-use teams::{Team, TEAMS};
-use tokio::{sync::RwLock, task::JoinSet};
+use std::{fs, time::SystemTime};
+use teams::Team;
+use tokio::sync::RwLock;
 
-const TIMEOUT_MS: u16 = 2000;
 const STALE_SECONDS: i64 = 60;
 const MAX_STALE_SECONDS: i64 = 60 * 60;
-
-#[derive(Copy, Clone, PartialEq, Eq)]
-enum Protocol {
-    Http,
-    Https,
-}
-
-impl Protocol {
-    fn as_str(&self) -> &str {
-        match self {
-            Protocol::Http => "http",
-            Protocol::Https => "https",
-        }
-    }
-}
-
-enum RequestResult {
-    Ok(u16),
-    NginxDefaultPage(u16),
-    CorrectRedirect(u16),
-    IncorrectRedirect(u16),
-    UnexpectedResponse(u16),
-    Timeout,
-    UntrustedCertificate,
-    FailedConnect,
-    Error(reqwest::Error),
-}
 
 #[derive(Clone, Serialize)]
 #[serde(tag = "type")]
@@ -110,9 +82,9 @@ impl RequestResultTemplate {
     }
 }
 
-impl RequestResult {
-    fn into_template(self, protocol: Protocol) -> RequestResultTemplate {
-        match self {
+impl RequestResultTemplate {
+    fn from_result(request_result: RequestResult, protocol: Protocol) -> RequestResultTemplate {
+        match request_result {
             RequestResult::Ok(status_code) => RequestResultTemplate::new(
                 RequestResultResponseTemplate::Ok { status_code },
                 match protocol {
@@ -162,12 +134,6 @@ impl RequestResult {
     }
 }
 
-struct TeamResult {
-    team: Team,
-    http: RequestResult,
-    https: RequestResult,
-}
-
 #[derive(Clone, Serialize)]
 struct TeamResultTemplate {
     team: Team,
@@ -179,122 +145,10 @@ impl From<TeamResult> for TeamResultTemplate {
     fn from(team_result: TeamResult) -> Self {
         TeamResultTemplate {
             team: team_result.team,
-            http: team_result.http.into_template(Protocol::Http),
-            https: team_result.https.into_template(Protocol::Https),
+            http: RequestResultTemplate::from_result(team_result.http, Protocol::Http),
+            https: RequestResultTemplate::from_result(team_result.https, Protocol::Https),
         }
     }
-}
-
-async fn check_protocol(client: reqwest::Client, team: &Team, protocol: Protocol) -> RequestResult {
-    let url = format!("{}://{}/", protocol.as_str(), team.domain);
-    match client
-        .get(&url)
-        .timeout(Duration::from_millis(TIMEOUT_MS as u64))
-        .send()
-        .await
-    {
-        Ok(response) => {
-            if response.status() == reqwest::StatusCode::OK {
-                // OK
-                let status_code = response.status().as_u16();
-                if response.text().await.unwrap().contains(
-                    "<p>If you see this page, the nginx web server is successfully installed and\nworking. Further configuration is required.</p>",
-                ) {
-                    RequestResult::NginxDefaultPage(status_code)
-                } else {
-                    RequestResult::Ok(status_code)
-                }
-            } else if matches!(
-                response.status(),
-                reqwest::StatusCode::MOVED_PERMANENTLY
-                    | reqwest::StatusCode::PERMANENT_REDIRECT
-                    | reqwest::StatusCode::FOUND
-            ) && protocol == Protocol::Http
-                && response.headers().contains_key("Location")
-            {
-                // Redirect from HTTP
-                if response
-                    .headers()
-                    .get("Location")
-                    .unwrap()
-                    .to_str()
-                    .unwrap()
-                    == format!("https://{}/", team.domain)
-                {
-                    // Redirects correctly
-                    RequestResult::CorrectRedirect(response.status().as_u16())
-                } else {
-                    // Redirects incorrectly
-                    RequestResult::IncorrectRedirect(response.status().as_u16())
-                }
-            } else {
-                RequestResult::UnexpectedResponse(response.status().as_u16())
-            }
-        }
-        Err(e) => {
-            if e.is_timeout() {
-                eprintln!("Timeout: {}", url);
-                RequestResult::Timeout
-            } else if e.is_connect() {
-                if protocol == Protocol::Https
-                    && format!("{}", e).contains("The certificate was not trusted")
-                {
-                    RequestResult::UntrustedCertificate
-                } else {
-                    RequestResult::FailedConnect
-                }
-            } else {
-                RequestResult::Error(e)
-            }
-        }
-    }
-}
-
-async fn get_results() -> Vec<TeamResult> {
-    for _ in 0..2 {
-        let mut results = vec![];
-        let mut join_set = JoinSet::new();
-        let client = reqwest::Client::builder()
-            // .proxy(reqwest::Proxy::all("socks5://127.0.0.1:9090").unwrap())
-            .redirect(reqwest::redirect::Policy::none())
-            .user_agent("https://uob-team-project-2022.nihaal.dev/")
-            .min_tls_version(reqwest::tls::Version::TLS_1_2)
-            .build()
-            .unwrap();
-        for team in TEAMS {
-            let client = client.clone();
-            join_set.spawn(async move {
-                let (http_result, https_result) = tokio::join!(
-                    check_protocol(client.clone(), team, Protocol::Http),
-                    check_protocol(client.clone(), team, Protocol::Https)
-                );
-                TeamResult {
-                    team: team.clone(),
-                    http: http_result,
-                    https: https_result,
-                }
-            });
-        }
-        while let Some(handle) = join_set.join_next().await {
-            results.push(handle.unwrap());
-        }
-
-        if results.iter().all(|result| {
-            matches!(
-                result.http,
-                RequestResult::Timeout | RequestResult::FailedConnect
-            ) && matches!(
-                result.https,
-                RequestResult::Timeout | RequestResult::FailedConnect
-            )
-        }) {
-            tokio::time::sleep(Duration::from_secs(1)).await;
-            continue;
-        }
-
-        return results;
-    }
-    panic!("connection failed");
 }
 
 #[derive(Clone)]
